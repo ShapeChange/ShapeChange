071e091f18b52b1b8f84f8286d64e3688a96db18
Johannes Echterhoff <echterhoff@interactive-instruments.de>
2020-08-10 08:22:37 +0200

    Remove 32bit check in basic configuration validator
    Signed-off-by: Johannes Echterhoff <echterhoff@interactive-instruments.de>

--- src/main/java/de/interactive_instruments/ShapeChange/BasicConfigurationValidator.java
+++ src/main/java/de/interactive_instruments/ShapeChange/BasicConfigurationValidator.java
@@ -47,234 +47,217 @@ import com.google.common.base.Joiner;
 import de.interactive_instruments.ShapeChange.Target.TargetOutputProcessor;
 
 /**
- * @author Johannes Echterhoff (echterhoff at interactive-instruments dot
- *         de)
+ * @author Johannes Echterhoff (echterhoff at interactive-instruments dot de)
  *
  */
 public class BasicConfigurationValidator implements MessageSource {
 
-	/**
-	 * Validates the 'input'-section of the ShapeChange configuration. Any
-	 * invalidity is directly logged in the ShapeChangeResult.
+    /**
+     * Validates the 'input'-section of the ShapeChange configuration. Any
+     * invalidity is directly logged in the ShapeChangeResult.
+     * 
+     * @param options tbd
+     * @param result  tbd
+     * @return <code>true</code> if the configuration is valid, else
+     *         <code>false</code>
+     */
+    public boolean isValid(Options options, ShapeChangeResult result) {
+
+	boolean isValid = true;
+
+	String imt = options.parameter("inputModelType");
+
+	if (imt == null) {
+	    result.addProcessFlowError(null, 26);
+	    isValid = false;
+	}
+
+	/*
+	 * If the input type is EA7 and we are not only executing deferrable output
+	 * writers, check that we are running on 32bit Java in a windows environment.
 	 * 
-	 * @param options tbd
-	 * @param result tbd
-	 * @return <code>true</code> if the configuration is valid, else
-	 *         <code>false</code>
+	 * NOTE: Apparently, it is not trivial to detect if a java program is executed
+	 * with 32bit or 64bit JRE. The web has many suggestions on how to do it. Here,
+	 * we make the assumption that in order to execute Enterprise Architect (EA),
+	 * the JRE must be run in Windows. Under that assumption, the java system
+	 * property 'os.arch' apparently is always 'x86' if the program is run with a
+	 * 32bit JRE.
 	 */
-	public boolean isValid(Options options, ShapeChangeResult result) {
+	if (imt.equalsIgnoreCase("EA7") && !options.isOnlyDeferrableOutputWrite()) {
 
-		boolean isValid = true;
+	    /*
+	     * 2020-08-10 JE: Since v15, Enterprise Architect has a 64bit dll, so can be run
+	     * with 64bit Java. Therefore, this check is no longer required.
+	     */
+//			boolean isWindows = SystemUtils.IS_OS_WINDOWS;
+//			String osArch = SystemUtils.OS_ARCH;
+//
+//			if (!isWindows) {
+//				result.addProcessFlowError(this, 1);
+//				isValid = false;
+//			} else if (!osArch.equalsIgnoreCase("x86")) {
+//				result.addProcessFlowError(this, 2, osArch);
+//				isValid = false;
+//			}
+	}
 
-		String imt = options.parameter("inputModelType");
+	/* === check output processing parameters === */
+	List<TargetConfiguration> targetConfigs = options.getTargetConfigurations();
 
-		if (imt == null) {
-			result.addProcessFlowError(null, 26);
+	for (TargetConfiguration conf : targetConfigs) {
+
+	    boolean applyXslt = conf.parameterAsBoolean(TargetOutputProcessor.PARAM_APPLY_XSLT, false);
+
+	    if (applyXslt) {
+
+		Joiner joiner = Joiner.on(", ");
+
+		String pathToXsltDirectory = conf.parameterAsString(TargetOutputProcessor.PARAM_PATH_TO_XSLT_DIRECTORY,
+			".", false, true);
+		String xsltFileName = conf.parameterAsString(TargetOutputProcessor.PARAM_XSLT_FILENAME, null, false,
+			true);
+
+		if (xsltFileName == null) {
+
+		    result.addProcessFlowError(this, 102, conf.getClassName(), joiner.join(conf.getInputIds()));
+		    isValid = false;
+
+		} else if (pathToXsltDirectory.toLowerCase().startsWith("http")) {
+
+		    String urlString = pathToXsltDirectory + "/" + xsltFileName;
+		    try {
+			URL url = new URL(urlString);
+			url.toURI();
+		    } catch (URISyntaxException | MalformedURLException e) {
+			result.addProcessFlowError(this, 100, conf.getClassName(), joiner.join(conf.getInputIds()),
+				urlString, e.getMessage());
 			isValid = false;
+		    }
+
+		} else {
+
+		    File xsl = new File(pathToXsltDirectory + "/" + xsltFileName);
+		    if (!xsl.exists()) {
+			result.addProcessFlowError(this, 101, conf.getClassName(), joiner.join(conf.getInputIds()),
+				xsl.getAbsolutePath());
+			isValid = false;
+		    }
 		}
-
-		/*
-		 * If the input type is EA7 and we are not only executing deferrable
-		 * output writers, check that we are running on 32bit Java in a windows
-		 * environment.
-		 * 
-		 * NOTE: Apparently, it is not trivial to detect if a java program is
-		 * executed with 32bit or 64bit JRE. The web has many suggestions on how
-		 * to do it. Here, we make the assumption that in order to execute
-		 * Enterprise Architect (EA), the JRE must be run in Windows. Under that
-		 * assumption, the java system property 'os.arch' apparently is always
-		 * 'x86' if the program is run with a 32bit JRE.
-		 */
-		if (imt.equalsIgnoreCase("EA7")
-				&& !options.isOnlyDeferrableOutputWrite()) {
-
-			boolean isWindows = SystemUtils.IS_OS_WINDOWS;
-			String osArch = SystemUtils.OS_ARCH;
-
-			if (!isWindows) {
-				result.addProcessFlowError(this, 1);
-				isValid = false;
-			} else if (!osArch.equalsIgnoreCase("x86")) {
-				result.addProcessFlowError(this, 2, osArch);
-				isValid = false;
-			}
-		}
-
-		/* === check output processing parameters === */
-		List<TargetConfiguration> targetConfigs = options
-				.getTargetConfigurations();
-
-		for (TargetConfiguration conf : targetConfigs) {
-
-			boolean applyXslt = conf.parameterAsBoolean(
-					TargetOutputProcessor.PARAM_APPLY_XSLT, false);
-
-			if (applyXslt) {
-
-				Joiner joiner = Joiner.on(", ");
-
-				String pathToXsltDirectory = conf.parameterAsString(
-						TargetOutputProcessor.PARAM_PATH_TO_XSLT_DIRECTORY, ".",
-						false, true);
-				String xsltFileName = conf.parameterAsString(
-						TargetOutputProcessor.PARAM_XSLT_FILENAME, null, false,
-						true);
-
-				if (xsltFileName == null) {
-
-					result.addProcessFlowError(this, 102, conf.getClassName(),
-							joiner.join(conf.getInputIds()));
-					isValid = false;
-
-				} else if (pathToXsltDirectory.toLowerCase()
-						.startsWith("http")) {
-
-					String urlString = pathToXsltDirectory + "/" + xsltFileName;
-					try {
-						URL url = new URL(urlString);
-						url.toURI();
-					} catch (URISyntaxException | MalformedURLException e) {
-						result.addProcessFlowError(this, 100, conf.getClassName(),
-								joiner.join(conf.getInputIds()), urlString,
-								e.getMessage());
-						isValid = false;
-					}
-
-				} else {
-
-					File xsl = new File(
-							pathToXsltDirectory + "/" + xsltFileName);
-					if (!xsl.exists()) {
-						result.addProcessFlowError(this, 101, conf.getClassName(),
-								joiner.join(conf.getInputIds()),
-								xsl.getAbsolutePath());
-						isValid = false;
-					}
-				}
-			}
-		}
-
-		/* === Validate descriptor sources === */
-		SortedMap<String, String> descriptorSources = options.getInputConfig()
-				.getDescriptorSources();
-		for (Entry<String, String> entry : descriptorSources.entrySet()) {
-
-			/*
-			 * Value is either 'sc:extract#sometoken' or 'tag#sometag'
-			 */
-			String[] components = StringUtils
-					.splitPreserveAllTokens(entry.getValue(), "#");
-			if (components[0].equalsIgnoreCase("sc:extract")
-					&& components[1].length() == 0) {
-
-				result.addProcessFlowError(this, 200, entry.getKey());
-				isValid = false;
-
-			} else if (components[0].equalsIgnoreCase("tag")
-					&& components[1].length() == 0) {
-
-				result.addProcessFlowError(this, 201, entry.getKey());
-				isValid = false;
-			}
-		}
-
-		/*
-		 * ====== Check XmlSchema target configuration ======
-		 */
-		for (TargetConfiguration conf : targetConfigs) {
-
-			if (conf instanceof TargetXmlSchemaConfiguration) {
-
-				TargetXmlSchemaConfiguration xsdConf = (TargetXmlSchemaConfiguration) conf;
-
-				List<XsdMapEntry> xsdMapEntries = xsdConf.getXsdMapEntries();
-
-				for (XsdMapEntry xsdme : xsdMapEntries) {
-
-					if (xsdme.getXmlElementHasSimpleContent() != null
-							&& !xsdme.hasXmlElement()) {
-						result.addProcessFlowError(this, 300, xsdme.getType(),
-								String.join(" ", xsdme.getEncodingRules()));
-						isValid = false;
-					}
-				}
-
-				for (XsdPropertyMapEntry xpme : xsdConf
-						.getXsdPropertyMapEntries().values()) {
-
-					/*
-					 * Ensure that the targetElement contains a QName like
-					 * string.
-					 */
-					if (xpme.hasTargetElement()) {
-
-						String targetElement = xpme.getTargetElement();
-
-						if (StringUtils.countMatches(targetElement, ":") != 1) {
-
-							result.addProcessFlowError(this, 301, xpme.toString());
-							isValid = false;
-
-						} else {
-
-							String nsabr = targetElement.split(":")[0];
-							if (options.fullNamespace(nsabr) == null) {
-								result.addProcessFlowError(this, 302, xpme.toString(),
-										nsabr);
-								isValid = false;
-							}
-						}
-					}
-				}
-			}
-		}
-
-		return isValid;
+	    }
 	}
 
-	@Override
-	public String message(int mnr) {
+	/* === Validate descriptor sources === */
+	SortedMap<String, String> descriptorSources = options.getInputConfig().getDescriptorSources();
+	for (Entry<String, String> entry : descriptorSources.entrySet()) {
 
-		switch (mnr) {
-		case 0:
-			return "Context: class BasicConfigurationValidator";
-		case 1:
-			return "The input parameter 'inputModelType' is set to 'EA7'. When loading an Enterprise Architect model, ShapeChange must be executed in Windows OS. ShapeChange detected that it is run in a different OS.";
-		case 2:
-			return "The input parameter 'inputModelType' is set to 'EA7'. When loading an Enterprise Architect model, ShapeChange must be executed in Windows OS with a 32bit JRE. ShapeChange detected that it is not executed with a 32bit JRE. The value of system property 'os.arch' is: '$1$'.";
+	    /*
+	     * Value is either 'sc:extract#sometoken' or 'tag#sometag'
+	     */
+	    String[] components = StringUtils.splitPreserveAllTokens(entry.getValue(), "#");
+	    if (components[0].equalsIgnoreCase("sc:extract") && components[1].length() == 0) {
 
-		// 100-199: Validation of output processing parameters
-		case 100:
-			return "XSL transformation of output files is requested via configuration parameter '"
-					+ TargetOutputProcessor.PARAM_APPLY_XSLT
-					+ "' for target with class name '$1$' and input(s) '$2$'. The URL that results from concatenating the path to the XSL directory and the XSLT file name - '$3$' - could not be converted to a URI. Exception message is: '$4$'.";
-		case 101:
-			return "XSL transformation of output files is requested via configuration parameter '"
-					+ TargetOutputProcessor.PARAM_APPLY_XSLT
-					+ "' for target with class name '$1$' and input(s) '$2$'. No XSL file was found at location '$3$'.";
-		case 102:
-			return "XSL transformation of output files is requested via configuration parameter '"
-					+ TargetOutputProcessor.PARAM_APPLY_XSLT
-					+ "' for target with class name '$1$' and input(s) '$2$'. Required parameter '"
-					+ TargetOutputProcessor.PARAM_APPLY_XSLT
-					+ "' was not configured (or does not contain a non-empty value).";
+		result.addProcessFlowError(this, 200, entry.getKey());
+		isValid = false;
 
-		// 200-299: Validation of descriptor sources
-		case 200:
-			return "Source for descriptor '$1$' is 'sc:extract', but required token is not provided.";
-		case 201:
-			return "Source for descriptor '$1$' is 'tag', but required tag is not provided.";
+	    } else if (components[0].equalsIgnoreCase("tag") && components[1].length() == 0) {
 
-		// 300-399: Validation of XsdMapEntries and XsdPropertyMapEntries
-		case 300:
-			return "XsdMapEntry with @type '$1$' and @xsdEncodingRule '$2$' is invalid because @xmlElementHasSimpleContent is set but @xmlElement has no value.";
-		case 301:
-			return "XsdPropertyMapEntry '$1$' is invalid because the @targetElement is not a QName like string.";
-		case 302:
-			return "XsdPropertyMapEntry '$1$' is invalid because the configuration does not contain a namespace definition with the namespace prefix '$2$', which is used by the @targetElement.";
-		default:
-			return "(" + BasicConfigurationValidator.class.getName()
-					+ ") Unknown message with number: " + mnr;
-		}
+		result.addProcessFlowError(this, 201, entry.getKey());
+		isValid = false;
+	    }
 	}
+
+	/*
+	 * ====== Check XmlSchema target configuration ======
+	 */
+	for (TargetConfiguration conf : targetConfigs) {
+
+	    if (conf instanceof TargetXmlSchemaConfiguration) {
+
+		TargetXmlSchemaConfiguration xsdConf = (TargetXmlSchemaConfiguration) conf;
+
+		List<XsdMapEntry> xsdMapEntries = xsdConf.getXsdMapEntries();
+
+		for (XsdMapEntry xsdme : xsdMapEntries) {
+
+		    if (xsdme.getXmlElementHasSimpleContent() != null && !xsdme.hasXmlElement()) {
+			result.addProcessFlowError(this, 300, xsdme.getType(),
+				String.join(" ", xsdme.getEncodingRules()));
+			isValid = false;
+		    }
+		}
+
+		for (XsdPropertyMapEntry xpme : xsdConf.getXsdPropertyMapEntries().values()) {
+
+		    /*
+		     * Ensure that the targetElement contains a QName like string.
+		     */
+		    if (xpme.hasTargetElement()) {
+
+			String targetElement = xpme.getTargetElement();
+
+			if (StringUtils.countMatches(targetElement, ":") != 1) {
+
+			    result.addProcessFlowError(this, 301, xpme.toString());
+			    isValid = false;
+
+			} else {
+
+			    String nsabr = targetElement.split(":")[0];
+			    if (options.fullNamespace(nsabr) == null) {
+				result.addProcessFlowError(this, 302, xpme.toString(), nsabr);
+				isValid = false;
+			    }
+			}
+		    }
+		}
+	    }
+	}
+
+	return isValid;
+    }
+
+    @Override
+    public String message(int mnr) {
+
+	switch (mnr) {
+	case 0:
+	    return "Context: class BasicConfigurationValidator";
+	case 1:
+	    return "The input parameter 'inputModelType' is set to 'EA7'. When loading an Enterprise Architect model, ShapeChange must be executed in Windows OS. ShapeChange detected that it is run in a different OS.";
+	case 2:
+	    return "The input parameter 'inputModelType' is set to 'EA7'. When loading an Enterprise Architect model, ShapeChange must be executed in Windows OS with a 32bit JRE. ShapeChange detected that it is not executed with a 32bit JRE. The value of system property 'os.arch' is: '$1$'.";
+
+	// 100-199: Validation of output processing parameters
+	case 100:
+	    return "XSL transformation of output files is requested via configuration parameter '"
+		    + TargetOutputProcessor.PARAM_APPLY_XSLT
+		    + "' for target with class name '$1$' and input(s) '$2$'. The URL that results from concatenating the path to the XSL directory and the XSLT file name - '$3$' - could not be converted to a URI. Exception message is: '$4$'.";
+	case 101:
+	    return "XSL transformation of output files is requested via configuration parameter '"
+		    + TargetOutputProcessor.PARAM_APPLY_XSLT
+		    + "' for target with class name '$1$' and input(s) '$2$'. No XSL file was found at location '$3$'.";
+	case 102:
+	    return "XSL transformation of output files is requested via configuration parameter '"
+		    + TargetOutputProcessor.PARAM_APPLY_XSLT
+		    + "' for target with class name '$1$' and input(s) '$2$'. Required parameter '"
+		    + TargetOutputProcessor.PARAM_APPLY_XSLT
+		    + "' was not configured (or does not contain a non-empty value).";
+
+	// 200-299: Validation of descriptor sources
+	case 200:
+	    return "Source for descriptor '$1$' is 'sc:extract', but required token is not provided.";
+	case 201:
+	    return "Source for descriptor '$1$' is 'tag', but required tag is not provided.";
+
+	// 300-399: Validation of XsdMapEntries and XsdPropertyMapEntries
+	case 300:
+	    return "XsdMapEntry with @type '$1$' and @xsdEncodingRule '$2$' is invalid because @xmlElementHasSimpleContent is set but @xmlElement has no value.";
+	case 301:
+	    return "XsdPropertyMapEntry '$1$' is invalid because the @targetElement is not a QName like string.";
+	case 302:
+	    return "XsdPropertyMapEntry '$1$' is invalid because the configuration does not contain a namespace definition with the namespace prefix '$2$', which is used by the @targetElement.";
+	default:
+	    return "(" + BasicConfigurationValidator.class.getName() + ") Unknown message with number: " + mnr;
+	}
+    }
 }
